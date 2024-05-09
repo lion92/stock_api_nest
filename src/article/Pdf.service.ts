8de0a0081a163b4aca88fd1beb7e0a055586b4ed
lion92 <38532569+lion92@users.noreply.github.com>
2024-05-09 20:10:19 +0200

    valeur du stock en pdf

--- src/article/Pdf.service.ts
+++ src/article/Pdf.service.ts
@@ -16,16 +16,21 @@ export class PdfService {
     console.log(total)
     console.log(newVar1);
 
-    doc.text('Bilan');
+    doc.fill("red").text('Bilan');
 
     newVar1.forEach(value => rowsToPdf.push([value?.id.toString(), value?.stockref.toString(), value?.quantite.toString(), value?.nom.toString(), value?.description.toString(), value?.prix.toString(),value?.dateAjout.toString()]))
     const table={
       headers:["id", "ref","quantite","Nom","Description","Prix","Date Ajout"],
       rows:rowsToPdf
     }
-    
-    await doc.moveDown().table(table);
-    doc.text("Valeur total du stock: " + total[0].prix);
+    // Scale the image
+    // @ts-ignore
+    await doc.fill("blue").moveDown().table(table, {
+      prepareHeader: () => doc.font("Helvetica-Bold").fontSize(8)
+    ,
+    }
+      );
+    doc.fill("green").text("Valeur total du stock: " + total[0].prix);
     const fileName = 'bilan.pdf';
     doc.pipe(fs.createWriteStream(fileName));
     doc.end();
